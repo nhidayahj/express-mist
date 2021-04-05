63f27ec1f8af5fad1ec4d38c91a53a3dcc3c43af
nhidayahj <hidayah.joha@gmail.com>
2021-04-05 17:20:31 +0000

    added category r/s for diffuser table

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const bookshelf = require('./bookshelf');
+
+// create an instance of the model 
+// model instance == one row in the table 
+
+const Diffuser = bookshelf.model('Diffuser', {
+    tableName:'diffusers',
+    category(){
+        return this.belongsTo('Diffuser_Category')
+    }
+})
+
+const Diffuser_Category = bookshelf.model('Diffuser_Category', {
+    tableName:'diffuser_category',
+    diffuser() {
+        return this.hasMany('Diffuser')
+    }
+})
+
+module.exports = {Diffuser, Diffuser_Category}
